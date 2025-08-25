26dec247d2bbd98f7387db6d209d9c7fe71cc8df
Isuru Bandara <isurubandara318@gmail.com>
2025-08-25 10:00:09 +0530

    fd

--- database/migrations/2025_06_30_055804_create_shifts_table.php
+++ database/migrations/2025_06_30_055804_create_shifts_table.php
@@ -4,29 +4,41 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
-        Schema::table('shifts', function (Blueprint $table) {
-            $table->dropColumn([
-                'morning_ot_start',
-                'special_ot_start',
-                'late_deduction',
-                'nopay_hour_halfday',
-                'break_time'
-            ]);
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('shift_code')->unique();
+            $table->string('shift_description');
+
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->time('morning_ot_start')->nullable();
+            $table->time('special_ot_start')->nullable();
+            $table->time('late_deduction')->nullable();
+            $table->string('break_time')->nullable();
+
+            $table->boolean('midnight_roster')->default(false);
+            $table->decimal('nopay_hour_halfday', 4, 2)->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+
+
+            $table->index('shift_code');
+            $table->index('start_time');
+            $table->index('end_time');
+            $table->index('morning_ot_start');
+            $table->index('special_ot_start');
+            $table->index('late_deduction');
+
         });
     }
 
     public function down()
     {
-        Schema::table('shifts', function (Blueprint $table) {
-            $table->time('morning_ot_start')->nullable();
-            $table->time('special_ot_start')->nullable();
-            $table->time('late_deduction')->nullable();
-            $table->decimal('nopay_hour_halfday', 5, 2)->default(0);
-            $table->integer('break_time')->default(0);
-        });
+        Schema::dropIfExists('shifts');
     }
 };
