7ade483e015f994dc9ccc30dcb5e2f5f21226788
DilanSriyantha <56430055+DilanSriyantha@users.noreply.github.com>
2026-03-18 19:25:42 +0530

    Refactor column existence checks in migration

--- database/migrations/2026_03_17_045241_add_short_leave_columns_to_leave_masters_table.php
+++ database/migrations/2026_03_17_045241_add_short_leave_columns_to_leave_masters_table.php
@@ -11,17 +11,17 @@ return new class extends Migration
         Schema::table('leave_masters', function (Blueprint $table) {
             
             // is_short_leave check karala add kireema
-            if (!Schema::hasColumn('leave_masters', 'is_short_leave')) {
+            if (!Schema::hasColumn('is_short_leave')) {
                 $table->boolean('is_short_leave')->default(0)->nullable()->after('is_half_day');
             }
 
             // short_leave_slot check karala add kireema
-            if (!Schema::hasColumn('leave_masters', 'short_leave_slot')) {
+            if (!Schema::hasColumn('short_leave_slot')) {
                 $table->string('short_leave_slot')->nullable()->after('is_short_leave');
             }
 
             // period check karala add kireema
-            if (!Schema::hasColumn('leave_masters', 'period')) {
+            if (!Schema::hasColumn('period')) {
                 $table->string('period')->nullable()->after('short_leave_slot');
             }
         });
