722e66f24899bc3b15b1873f57b7c3c6ce04cae9
Chamod-alt <152728704+Chamod-alt@users.noreply.github.com>
2026-03-18 20:59:10 +0530

    change the name of model roster page

--- app/Models/Resignation.php
+++ app/Models/Resignation.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Resignation extends Model
 {
     use HasFactory, SoftDeletes;
-
+//
     protected $fillable = [
         'employee_id',
         'resigning_date',
--- app/Models/roster.php
+++ app/Models/roster.php
@@ -47,7 +47,7 @@ class Roster extends Model
         return $this->belongsTo(sub_departments::class);
     }
 
-    //relation for employee
+    //relation for employe
     public function employee()
     {
         return $this->belongsTo(employee::class);
